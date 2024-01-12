757c6b0603826da9204939d2ac392e9873f4fb4a
Matheus Vicente <mv_vieira96@hotmail.com>
2024-01-12 16:57:05 -0300

    Implementação do producer no projeto User

--- user/src/main/java/com/ms/user/producer/UserProducer.java
+++ user/src/main/java/com/ms/user/producer/UserProducer.java
@@ -1,7 +1,36 @@
 package com.ms.user.producer;
 
+import com.ms.user.dtos.EmailRecordDto;
+import com.ms.user.models.UserModel;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserProducer {
+
+    final RabbitTemplate rabbitTemplate;
+
+    public UserProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Value(value = "${broker.queue.email.name}")
+    private String routingKey;
+
+    public void publishMessageEmail(UserModel userModel) {
+
+        String subject = "Cadastro realizado com sucesso";
+        String text = userModel.getName() + ", Seja Bem Vindo(a)!\n"+ "Agradecemos o seu cadastro, aproveite agora"
+                + " todos os recursos da nossa plataforma!";
+
+        var emailRecordDto = new EmailRecordDto(
+                userModel.getUserId(),
+                userModel.getEmail(),
+                subject,
+                text
+        );
+
+        rabbitTemplate.convertAndSend("", routingKey, emailRecordDto);
+    }
 }
--- user/src/main/java/com/ms/user/services/UserService.java
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ms.user.services;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.producer.UserProducer;
 import com.ms.user.repositories.UserRepository;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +12,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     final UserRepository userRepository;
+    final UserProducer userProducer;
 
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository, UserProducer userProducer){
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
 
     @Transactional
     public UserModel save(UserModel userModel){
-       return userRepository.save(userModel);
+        userModel = userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return userModel;
     }
 }
