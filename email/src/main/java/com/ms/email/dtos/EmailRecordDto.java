8278b2c2eeee147078ab87dbad577182a846d438
Matheus Vicente <mv_vieira96@hotmail.com>
2024-01-12 17:41:42 -0300

    Adicionando annotations @JsonIgnoreProperties no EmailRecordDto

--- email/src/main/java/com/ms/email/dtos/EmailRecordDto.java
+++ email/src/main/java/com/ms/email/dtos/EmailRecordDto.java
@@ -1,7 +1,10 @@
 package com.ms.email.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.UUID;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record EmailRecordDto(UUID id,
                              String emailTo,
                              String subject,
